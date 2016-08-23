219287ad80e586e00ed02465a660f71be13b08bd
vuuihc <zshyou@gmail.com>
2016-08-23 18:20:14 +0800

    u

---
+++ src/javascripts/config.js
@@ -0,0 +1,3 @@
+const WebVRConfig = {
+  BUFFER_SCALE: 0.5, // default: 1.0
+};
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+/* eslint-disable*/
+var path = require('path')
+var webpack = require('webpack')
+var isProduction = function () {
+  return process.env.NODE_ENV === 'production';
+};
+
+var plugins = [];
+//"webpack-dev-server/client?http://localhost:8080/",
+if(isProduction()){
+  plugins.push(
+      new webpack.optimize.UglifyJsPlugin({
+        compress:{
+          warnings: false
+        }
+      })
+  )
+}else{
+  plugins.push(
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  )
+}
+module.exports = {
+	devtool: 'inline-source-map',
+	entry:{
+		VRPuzzle: __dirname+'/src/javascripts/index.js',
+	},
+	output:{
+		path:__dirname+'/public',
+		publicPath:'/public/',
+		filename:"[name].bundle.js"
+	},
+	module:{
+		loaders: [
+		  {
+			test: /\.jsx?$/,
+			loader  : ['babel'],
+			include: path.join(__dirname,'src'),
+			exclude: path.join(__dirname,'node_modules'),
+			query: {
+	          presets: ['es2015','es2017']
+	        }
+		  },
+		  {
+			test: /\.s?css$/,
+			loader: 'style!css!sass'
+		  },
+		  {
+			test:/\.(png|jpg|bmp)$/,
+			loader: 'url?limit=5000&name=images/[name].[ext]'
+		  }
+		]
+	},
+	plugins: plugins,
+	externals:{
+        // 'babel-polyfill': 'true',
+        // 'react': 'React',
+        // 'react-dom': 'ReactDOM',
+		// 'antd':'antd'
+    }
+}
