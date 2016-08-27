0a114d0e7546cd1a039e10baae9b15c1402d04c7
professorz <1172617666@qq.com>
2016-08-27 22:46:13 +0800

    'update'

--- src/javascripts/mouse.js
+++ src/javascripts/mouse.js
@@ -1,13 +1,13 @@
-export default class Mouse extend THREE.Vector2{
+export default class Mouse extends THREE.Vector2{
     constructor(props){
         super(props)
-        window.addEventListener( 'mousemove', this.onMouseMove.bind(this), false );
+        window.addEventListener( "click", this.onMouseClick.bind(this), false )
 
     }
-    onMouseMove( event ) {
+    onMouseClick( event ) {
     	// calculate mouse position in normalized device coordinates
     	// (-1 to +1) for both components
-    	this.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-    	this.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+        this.x = ( event.clientX / window.innerWidth ) * 2 - 1
+        this.y = - ( event.clientY / window.innerHeight ) * 2 + 1
     }
 }
